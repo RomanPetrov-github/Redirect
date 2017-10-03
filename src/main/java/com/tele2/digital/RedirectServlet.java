f2c4617f5a22f4a421684e9a8b6f7d690cd5fcdb
Roman Petrov <dev.petrov.roman@gmail.com>
2017-10-03 11:17:17 +0300

    Implement authorization

--- src/main/java/com/tele2/digital/RedirectServlet.java
+++ src/main/java/com/tele2/digital/RedirectServlet.java
@@ -1,8 +1,12 @@
 package com.tele2.digital;
 
+import com.tele2.digital.database.DBWorker;
 import com.tele2.digital.pojo.Incident;
 import com.tele2.digital.pojo.IncidentBuilder;
+import com.tele2.digital.pojo.Redirect;
 import com.tele2.digital.util.ValidatorURL;
+import org.apache.commons.codec.binary.Base64;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,33 +16,47 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class RedirectServlet extends HttpServlet{
+
+    public static boolean validateCredentials(String userCredentials) {
+        String login = "admin";
+        String password = "admin123";
+        String validCredentials = "Basic " + Base64.encodeBase64String((login + ":" + password).getBytes());
+        return userCredentials.equals(validCredentials);
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter out = resp.getWriter();
-        out.print("<h1>Hello Servlet</h1>");
-        System.out.println("=================================================================");
 
-        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-                "<incident>\n" +
-                "   <author>Смердов Александр Сергеевич</author>\n" +
-                "   <number>SR00819068</number>\n" +
-                "   <description>Редирект с раздела «Тарифы» в Карелиию. Задача срочная.</description>\n" +
-                "   <redirect>karelia.tele2.ru/tariffs/ karelia.tele2.ru/tariff/my-choice-300-10 302\n" +
-                "   orel.tele2.ru/tariffs/ orel.tele2.ru/tariff/my-choice-300-10 302</redirect>\n" +
-                "   <link>https://bpm.tele2.ru/0/Nui/ViewModule.aspx#CardModuleV2/CasePage/edit/281bb33a-75a8-465a-806b-00ca3a138df2</link>\n" +
-                "</incident>";
-        try
-        {
-            Incident incident = new IncidentBuilder(xml).build();
-            System.out.println("list size = " + incident.getListRedirects().size());
-            ValidatorURL.validateURL(incident);
+        if (validateCredentials(req.getHeader("Authorization"))){
+            resp.sendError(401);
+            return;
+        }
+
+//        PrintWriter out = resp.getWriter();
+//        out.print("<h1>Hello Servlet</h1>");
+//        System.out.println("=================================================================");
+//
+//        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+//                "<incident>\n" +
+//                "   <author>Смердов Александр Сергеевич</author>\n" +
+//                "   <number>SR00819068</number>\n" +
+//                "   <description>Редирект с раздела «Тарифы» в Карелиию. Задача срочная.</description>\n" +
+//                "   <redirect>karelia.tele2.ru/tariffs/ karelia.tele2.ru/tariff/my-choice-300-10 302\n" +
+//                "   orel.tele2.ru/tariffs/ orel.tele2.ru/tariff/my-choice-300-10 302</redirect>\n" +
+//                "   <link>https://bpm.tele2.ru/0/Nui/ViewModule.aspx#CardModuleV2/CasePage/edit/281bb33a-75a8-465a-806b-00ca3a138df2</link>\n" +
+//                "</incident>";
+//        try
+//        {
+//            Incident incident = new IncidentBuilder(xml).build();
+//            System.out.println("list size = " + incident.getListRedirects().size());
+//            ValidatorURL.validateURL(incident);
 //            for(Redirect redirect: incident.getListRedirects()){
 //                DBWorker.saveRedirect(redirect);
 //            }
-        }
-        catch (JAXBException e) {
-            e.printStackTrace();
-        }
+//        }
+//        catch (JAXBException e) {
+//            e.printStackTrace();
+//        }
     }
 
 }
